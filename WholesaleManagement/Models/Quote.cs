9f29ada49aad285ae26a575a3d7af43a1fd2009e
EL MEHDI EL AFAR <el-mehdi.el-afar@capgemini.com>
2024-08-06 20:33:27 +0100

    #Define Quote Request and Response Models
    Create models to represent the quote request and response

---
+++ WholesaleManagement/Models/Quote.cs
@@ -0,0 +1,15 @@
+﻿namespace WholesaleManagement.Models
+{
+    public class QuoteRequest
+    {
+        public int WholesalerId { get; set; }
+        public Dictionary<int, int> BeerOrders { get; set; } // Key: BeerId, Value: Quantity
+    }
+
+    public class QuoteResponse
+    {
+        public decimal TotalPrice { get; set; }
+        public string Summary { get; set; }
+    }
+
+}
