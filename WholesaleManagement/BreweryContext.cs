00ae9e0bc89250e2ca8f213aeedc467904c616c3
EL MEHDI EL AFAR <el-mehdi.el-afar@capgemini.com>
2024-08-06 20:20:21 +0100

    #Set Up the Database Context
    Create a DbContext to manage the database connection and entity sets:

---
+++ WholesaleManagement/BreweryContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using WholesaleManagement.Models;
+
+namespace WholesaleManagement
+{
+    public class BreweryContext : DbContext
+    {
+        public DbSet<Brewery> Breweries { get; set; }
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Wholesaler> Wholesalers { get; set; }
+        public DbSet<WholesalerStock> WholesalerStocks { get; set; }
+
+        public BreweryContext(DbContextOptions<BreweryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define relationships and constraints here if necessary
+        }
+    }
+
+}
