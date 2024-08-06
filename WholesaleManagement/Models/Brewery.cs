d0b8771c68616cb76be9567d1c76e577b1df99c9
EL MEHDI EL AFAR <el-mehdi.el-afar@capgemini.com>
2024-08-06 20:16:36 +0100

    #Project Setup and Installing Entity Framework Core # Adding models Create models to represent the entities in your system:     Brewery     Beer     Wholesaler     WholesalerStock (for managing stock levels)

---
+++ WholesaleManagement/Models/Brewery.cs
@@ -0,0 +1,38 @@
+﻿namespace WholesaleManagement.Models
+{
+    public class Brewery
+    {
+        public int BreweryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Beer> Beers { get; set; }
+    }
+
+    public class Beer
+    {
+        public int BeerId { get; set; }
+        public string Name { get; set; }
+        public double AlcoholContent { get; set; }
+        public decimal Price { get; set; }
+        public int BreweryId { get; set; }
+        public Brewery Brewery { get; set; }
+        public ICollection<WholesalerStock> WholesalerStocks { get; set; }
+    }
+
+    public class Wholesaler
+    {
+        public int WholesalerId { get; set; }
+        public string Name { get; set; }
+        public ICollection<WholesalerStock> WholesalerStocks { get; set; }
+    }
+
+    public class WholesalerStock
+    {
+        public int WholesalerStockId { get; set; }
+        public int WholesalerId { get; set; }
+        public Wholesaler Wholesaler { get; set; }
+        public int BeerId { get; set; }
+        public Beer Beer { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
